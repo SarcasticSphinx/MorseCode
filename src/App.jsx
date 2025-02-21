15bb8d97b45dfc452ef0346064f1cb7e26ede507
Suhail Ahmed Toha <2203038@student.ruet.ac.bd>
2025-02-21 20:09:30 +0600

    added routes and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import MorseConverter from "./Converter/MorseConverter";
 import MorseHomePage from "./Home Page/Home.jsx";
 import MorseExam from "./Morse Exam/MorseExam.jsx";
+import NavBar from "./Components/NavBar.jsx";
+
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <MorseHomePage />
-      <MorseExam />
-      <MorseConverter />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<MorseHomePage />} />
+        <Route path="/converter" element={<MorseConverter />} />
+        <Route path="/exam" element={<MorseExam />} />
+      </Routes>
     </>
   );
 }
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <nav className="navbar">
+            <div className="nav-container">
+                <h1 className="logo">Morse Code App</h1>
+                <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+                    ☰
+                </button>
+                <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+                    <Link to="/">Home</Link>
+                    <Link to="/converter">Converter</Link>
+                    <Link to="/exam">Exam</Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
