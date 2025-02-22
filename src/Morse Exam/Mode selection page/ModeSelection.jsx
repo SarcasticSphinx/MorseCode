6c9c590fcc64ce45eff5b2477ac37962ef407b6c
Suhail Ahmed Toha <2203038@student.ruet.ac.bd>
2025-02-22 19:15:05 +0600

    normalled again

--- src/Morse Exam/Mode selection page/ModeSelection.jsx
+++ src/Morse Exam/Mode selection page/ModeSelection.jsx
@@ -1,21 +1,17 @@
-import React from "react";
-import "./ModeSelection.css";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './ModeSelection.css';
+import { Link } from 'react-router-dom';
 const ModeSelection = () => {
-  return (
-    <div className="mode-selection">
-      <h1>Choose your mode:</h1>
-      <Link to="/MorseCode/exam/easy">
-        <button className="mode-button">Easy</button>
-      </Link>
-      <Link to="/MorseCode/exam/medium">
-        <button className="mode-button">Medium</button>
-      </Link>
-      <Link to="/MorseCode/exam/hard">
-        <button className="mode-button">Hard</button>
-      </Link>
-    </div>
-  );
+
+    return (
+        <div className="mode-selection">
+            <h1>Choose your mode:</h1>
+            <Link to="/MorseCode/exam/easy"><button className="mode-button">Easy</button></Link>
+<Link to="/MorseCode/exam/medium"><button className="mode-button">Medium</button></Link>
+<Link to="/MorseCode/exam/hard"><button className="mode-button">Hard</button></Link>
+
+        </div>
+    );
 };
 
 export default ModeSelection;
