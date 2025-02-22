d47f6290fd71e81ed3a597a323be4f6c844c9d77
Suhail Ahmed Toha <2203038@student.ruet.ac.bd>
2025-02-22 18:49:55 +0600

    added link instead of a

--- src/Morse Exam/Mode selection page/ModeSelection.jsx
+++ src/Morse Exam/Mode selection page/ModeSelection.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './ModeSelection.css';
+import { Link } from 'react-router-dom';
 const ModeSelection = () => {
 
     return (
         <div className="mode-selection">
             <h1>Choose your mode:</h1>
-            <a href="/MorseCode/exam/easy"><button className="mode-button">Easy</button></a>
-            <a href="/MorseCode/exam/medium"><button className="mode-button">Medium</button></a>
-            <a href="/MorseCode/exam/hard"><button className="mode-button">Hard</button></a>
+            <Link href="/MorseCode/exam/easy"><button className="mode-button">Easy</button></Link>
+            <Link href="/MorseCode/exam/medium"><button className="mode-button">Medium</button></Link>
+            <Link href="/MorseCode/exam/hard"><button className="mode-button">Hard</button></Link>
         </div>
     );
 };
