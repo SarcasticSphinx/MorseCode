daca3863effe7cb4f48be6874de7935fdaf4d73e
Suhail Ahmed Toha <2203038@student.ruet.ac.bd>
2025-02-22 18:55:54 +0600

    fixed some link issue

--- src/Morse Exam/Mode selection page/ModeSelection.jsx
+++ src/Morse Exam/Mode selection page/ModeSelection.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import './ModeSelection.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./ModeSelection.css";
+import { Link } from "react-router-dom";
 const ModeSelection = () => {
-
-    return (
-        <div className="mode-selection">
-            <h1>Choose your mode:</h1>
-            <Link href="/MorseCode/exam/easy"><button className="mode-button">Easy</button></Link>
-            <Link href="/MorseCode/exam/medium"><button className="mode-button">Medium</button></Link>
-            <Link href="/MorseCode/exam/hard"><button className="mode-button">Hard</button></Link>
-        </div>
-    );
+  return (
+    <div className="mode-selection">
+      <h1>Choose your mode:</h1>
+      <Link to="/MorseCode/exam/easy">
+        <button className="mode-button">Easy</button>
+      </Link>
+      <Link to="/MorseCode/exam/medium">
+        <button className="mode-button">Medium</button>
+      </Link>
+      <Link to="/MorseCode/exam/hard">
+        <button className="mode-button">Hard</button>
+      </Link>
+    </div>
+  );
 };
 
 export default ModeSelection;
